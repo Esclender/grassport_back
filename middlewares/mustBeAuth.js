c961b2d2cafa0b8d862368324f29ef29153cf3af
Esclender <elesclenderlugo@gmail.com>
2023-11-29 12:39:01 -0500

    hotfix: return res

--- middlewares/mustBeAuth.js
+++ middlewares/mustBeAuth.js
@@ -1,7 +1,7 @@
 async function mustBeAuthenticated (req, res, next) {
   const { isLogged } = req.jwt
   if (!isLogged) {
-    res.status(401).json({
+    return res.status(401).json({
       exitoso: false,
       message: 'Sin acceso a historial'
     })
