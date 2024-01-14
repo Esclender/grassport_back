8c04aea14d408b9c5d578b4eb6873fbc1cfbc9c8
Esclender <elesclenderlugo@gmail.com>
2024-01-13 21:04:36 -0500

    feat: web socket remove locations already visited

---
+++ utils/removeDuplicatesInArray.js
@@ -0,0 +1,11 @@
+function removeDuplicates (array1, array2) {
+  const uniquePlaceIds = new Set(array2.map(element => element.place_id.toString()))
+
+  console.log(uniquePlaceIds)
+
+  array1 = array1.filter(element => !uniquePlaceIds.has(element.place_id.toString()))
+
+  return array1
+}
+
+module.exports = removeDuplicates
